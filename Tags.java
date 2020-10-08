2f0e4f01bf7cd01b736acca3c679f2c5841cc957
Alan Cristian Martinez Genaro <martinezgenarocristian@gmail.com>
2020-10-08 07:26:31 -0500

    Agregando texto

--- Tags.java
+++ Tags.java
@@ -15,6 +15,7 @@ public class Tags {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.printl("Agregando texto")
         // TODO code application logic here
     }
     
